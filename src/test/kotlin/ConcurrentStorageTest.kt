f1f047cf1220ca1cc353d775a250e715032a375f
rusmonster <dkalita@twilio.com>
2024-07-14 11:41:16 +0300

    test: Add more tests for ConcurrentStorage

--- src/test/kotlin/ConcurrentStorageTest.kt
+++ src/test/kotlin/ConcurrentStorageTest.kt
@@ -1,10 +1,9 @@
 import org.example.storage.Storage
 import org.example.storage.newStorage
 import org.example.storage.synchronizedStorage
+import java.util.concurrent.atomic.AtomicReference
 import kotlin.concurrent.thread
-import kotlin.test.BeforeTest
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import kotlin.test.*
 
 class ConcurrentStorageTest {
 
@@ -13,6 +12,25 @@ class ConcurrentStorageTest {
     @BeforeTest
     fun setUp() {
         storage = Storage.synchronizedStorage(Storage.newStorage())
+    }
+
+    @Test
+    fun ensureBlockingRead() {
+        storage["foo"] = "0"
+        val value = AtomicReference<String>()
+        storage.beginTransaction()
+
+        val thread = thread {
+            value.set(storage["foo"])
+        }
+
+        Thread.sleep(1000)
+
+        storage["foo"] = "1"
+        storage.commitTransaction()
+
+        thread.join()
+        assertEquals("1", value.get())
     }
 
     @Test
@@ -66,5 +84,18 @@ class ConcurrentStorageTest {
         threads.forEach { it.join() }
 
         assertEquals("${numThreads * numIterations}", storage["foo"])
+    }
+
+    @Test
+    fun closeTransactionOnDifferentThread() {
+        val throwable = AtomicReference<Throwable>()
+        storage.beginTransaction()
+
+        val thread = thread {
+            val t = assertFails { storage.commitTransaction() }
+            throwable.set(t)
+        }
+        thread.join()
+        assertNotNull(throwable.get())
     }
 }
